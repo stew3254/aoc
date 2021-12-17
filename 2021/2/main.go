7f00c3a8aab350e2e42d6fed4e74a1dda0883076
stew3254 <rtstewy@gmail.com>
2021-12-17 09:29:23 -0500

    Finish day 2

---
+++ 2021/2/main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+)
+
+var URL = "https://adventofcode.com"
+
+func getAuth() *http.Cookie {
+	data, err := ioutil.ReadFile("../../auth.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return &http.Cookie{
+		Name:     "session",
+		Value:    string(data),
+		Path:     "/",
+		Domain:   ".adventofcode.com",
+		Secure:   true,
+		HttpOnly: true,
+	}
+}
+
+func makeClient() *http.Client {
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	urlObj, _ := url.Parse(URL)
+	jar.SetCookies(urlObj, []*http.Cookie{getAuth()})
+
+	return &http.Client{
+		Transport:     nil,
+		CheckRedirect: nil,
+		Jar:           jar,
+		Timeout:       0,
+	}
+}
+
+func getDocument(client *http.Client, url string) (*goquery.Document, error) {
+	// Get the HTML
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	} else if resp.StatusCode > 299 || resp.StatusCode < 200 {
+		return nil, errors.New(resp.Status)
+	}
+
+	// Convert HTML into goquery document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return doc, err
+	}
+	return doc, nil
+}
+
+func submit(client *http.Client, answer string) {
+	// Get the year and day from the path
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	p, day := path.Split(dir)
+	_, year := path.Split(p)
+
+	// Submit the answers
+	resp, err := client.PostForm(fmt.Sprintf("%s/%s/day/%s/answer", URL, year, day), url.Values{
+		"level":  {day},
+		"answer": {answer},
+	})
+
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// See if it's the right answer or not
+	if strings.Contains(doc.Find("main p").Text(), "not the right") {
+		log.Println("Wrong answer")
+	} else {
+		log.Println("Correct!")
+	}
+}
+
+type Pos struct {
+	Inst string
+	N    int
+}
+
+func getInput(scanner *bufio.Scanner) (output []Pos) {
+	// Scan the input text
+	for scanner.Scan() {
+		// Fail on error
+		err := scanner.Err()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// Use the text in the output
+		line := scanner.Text()
+		// Split the string into fields
+		parts := strings.Fields(line)
+		// Get number
+		n, _ := strconv.Atoi(parts[1])
+		output = append(output, Pos{
+			Inst: parts[0],
+			N:    n,
+		})
+	}
+	return output
+}
+
+func problem1(input []Pos) (output int) {
+	x := 0
+	y := 0
+	for _, p := range input {
+		// Switch on instruction
+		switch p.Inst {
+		case "forward":
+			x += p.N
+		case "up":
+			y += -p.N
+		case "down":
+			y += p.N
+		}
+	}
+	return x * y
+}
+
+func problem2(input []Pos) (output int) {
+	x := 0
+	y := 0
+	aim := 0
+	for _, p := range input {
+		// Switch on instruction
+		switch p.Inst {
+		case "forward":
+			x += p.N
+			y += p.N * aim
+		case "up":
+			aim += -p.N
+		case "down":
+			aim += p.N
+		}
+	}
+	return x * y
+}
+
+func main() {
+	// Read in the input for the day
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	reader := bufio.NewReader(file)
+	scanner := bufio.NewScanner(reader)
+
+	// Get the output to submit to the server
+	input := getInput(scanner)
+	sol1 := problem1(input)
+	log.Println("Solution 1:", sol1)
+	sol2 := problem2(input)
+	log.Println("Solution 2:", sol2)
+
+	// Send the output to the server
+	// client := makeClient()
+	// submit(client, sol1)
+	// submit(client, sol2)
+}
