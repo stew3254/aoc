da2ac7a2b37c5e1cbd0f8af58d032c61a920ba5f
stew3254 <rtstewy@gmail.com>
2021-12-19 10:42:48 -0500

    Day 5

--- 2021/5/main.go
+++ 2021/5/main.go
@@ -12,6 +12,7 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"strconv"
 	"strings"
 )
 
@@ -99,7 +100,76 @@ func submit(client *http.Client, answer string) {
 	}
 }
 
-func getInput(scanner *bufio.Scanner) (output []string) {
+func max(a, b int) int {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func abs(a int) int {
+	if a > -1 {
+		return a
+	} else {
+		return -a
+	}
+}
+
+type Point struct {
+	X int
+	Y int
+}
+
+type Line struct {
+	P1 Point
+	P2 Point
+}
+
+type Grid map[Point]int
+
+// Add the line to the grid
+func (grid *Grid) Add(l Line, diagonal bool) {
+	// Get point offsets
+	x := l.P2.X - l.P1.X
+	y := l.P2.Y - l.P1.Y
+
+	// Get signs for direction
+	xSign := 1
+	ySign := 1
+
+	if x < 0 {
+		xSign = -1
+	}
+	if y < 0 {
+		ySign = -1
+	}
+
+	for i := 0; i <= max(abs(x), abs(y)); i++ {
+		var point Point
+		if x != 0 && y != 0 {
+			// Assume the diagonals have a slope of 1
+			if diagonal {
+				point = Point{X: l.P1.X + i*xSign, Y: l.P1.Y + i*ySign}
+			} else {
+				// Skip the rest of this and don't add the point
+				continue
+			}
+		} else if x != 0 {
+			point = Point{X: l.P1.X + i*xSign, Y: l.P1.Y}
+		} else {
+			point = Point{X: l.P1.X, Y: l.P1.Y + i*ySign}
+		}
+		// If the point exists on the grid, bump up intersection number. Otherwise, set to 1
+		if _, exists := (*grid)[point]; exists {
+			(*grid)[point] += 1
+		} else {
+			(*grid)[point] = 1
+		}
+	}
+}
+
+func getInput(scanner *bufio.Scanner) (output []Line) {
 	// Scan the input text
 	for scanner.Scan() {
 		// Fail on error
@@ -110,16 +180,51 @@ func getInput(scanner *bufio.Scanner) (output []string) {
 
 		// Use the text in the output
 		line := scanner.Text()
-		output = append(output, line)
+		pointsStr := strings.Split(line, " -> ")
+		points := make([]Point, 0, 2)
+		for _, point := range pointsStr {
+			p := strings.Split(point, ",")
+			x, _ := strconv.Atoi(p[0])
+			y, _ := strconv.Atoi(p[1])
+			points = append(points, Point{X: x, Y: y})
+		}
+		l := Line{P1: points[0], P2: points[1]}
+		output = append(output, l)
 	}
 	return output
 }
 
-func problem1(input []string) (output int) {
+func problem1(input []Line) (output int) {
+	grid := make(Grid)
+
+	// Add all lines to the grid
+	for _, line := range input {
+		grid.Add(line, false)
+	}
+
+	// Get all points with intersections
+	for _, v := range grid {
+		if v > 1 {
+			output++
+		}
+	}
 	return output
 }
 
-func problem2(input []string) (output int) {
+func problem2(input []Line) (output int) {
+	grid := make(Grid)
+
+	// Add all lines to the grid
+	for _, line := range input {
+		grid.Add(line, true)
+	}
+
+	// Get all points with intersections
+	for _, i := range grid {
+		if i > 1 {
+			output++
+		}
+	}
 	return output
 }
 
